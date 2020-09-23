293722c153fbc516bf2b10cee484aac77f8f7a00
Timothy Do <tydo83@gmail.com>
2020-09-23 13:51:25 -0400

    chobani was my lunch

--- main.js
+++ main.js
@@ -2,9 +2,61 @@
  * YOUR CODE BELOW! *
  ********************/
 
+// More than 5
+function moreThan5(num){
+  return num > 5;
+}
 
+// Grade checker
+function isNewTopScore(score, topScore){
+  return score > topScore;
+}
+function isInDanger(grade){
+  return 71 >= grade && grade >= 60;
+}
+function isCoasting(grade){
+  return 83 >= grade && grade >= 72;
+}
+function isSucceeding(grade){
+  return 92 >= grade && grade >= 84;
+}
+function isFailing(grade){
+  return grade < 60;
+}
+function isAcing(grade){
+  return grade > 92;
+}
 
+// Role checker
+function isStudent(role){
+  return role === 'student';
+}
+function isTeacher(role){
+  return role === 'teacher';
+}
+function isAdmin(role){
+  return role === 'admin';
+}  
 
+// School level checker
+function isElementary(schoolLevel){
+  return schoolLevel === 'elementary';
+}
+
+// Different people checker
+function areDifferentPeople(name1, name2){
+  return name1 !== name2;
+}
+
+// Teacher checker
+function isMiddleSchoolTeacher(role, level){
+  return role == 'teacher' && 8 >= level && level >= 6;
+}
+
+// Administrator checker
+function notAnElementarySchoolAdministrator(schoolLevel, role){
+  return schoolLevel !== 'elementary' || role !== 'admin'
+}
 
 /********************************************************************************************
  * CODE BELOW IS FOR EXPORTING THE VARIABLES AND FUNCTIONS YOU WROTE ABOVE TO MAIN.TEST.JS. *
